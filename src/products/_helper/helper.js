fda4efb5d621f35ffa84fbb178a49db26dab1cba
Austin Finn <austin.finn@qantasloyalty.com>
2020-04-14 15:13:17 +1000

    Remove blank space

--- src/products/_helper/helper.js
+++ src/products/_helper/helper.js
@@ -5,9 +5,9 @@ const { anz, cba, nab, westpac } = config.products.hostNames
 async function getAllProducts() {
     let listOfPromises = []
     listOfPromises.push(nr.getRequest(`${anz}/cds-au/v1/banking/products`))
-    listOfPromises.push( nr.getRequest(`${cba}/cds-au/v1/banking/products`))
-    listOfPromises.push( nr.getRequest(`${nab}/cds-au/v1/banking/products`))
-    listOfPromises.push( nr.getRequest(`${westpac}/cds-au/v1/banking/products`))
+    listOfPromises.push(nr.getRequest(`${cba}/cds-au/v1/banking/products`))
+    listOfPromises.push(nr.getRequest(`${nab}/cds-au/v1/banking/products`))
+    listOfPromises.push(nr.getRequest(`${westpac}/cds-au/v1/banking/products`))
 
     const allResponses = await Promise.all(listOfPromises)
 
