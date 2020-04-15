99ae8d410905c43ffe71b91b9d91b0b6d439734c
Austin Finn <austin.finn@qantasloyalty.com>
2020-04-15 15:47:26 +1000

    Make a successful POST request and access properties in the Request body

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
-let router = require('express').Router()
+const router = require('express').Router()
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json()
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./utils/swagger/routeHandler');
+
+router.use(bodyParser.json());
 
 // products
 router.get('/v1/products/mortgages', require("./products/mortgages/mortgages"))
@@ -8,7 +12,7 @@ router.get('/v1/products/cards', require("./products/cards/cards"))
 
 // users
 router.get('/v1/users/loginCredentials', require('./users/loginCredentials/loginCredentials'))
-router.post('v1/users/create', require('./users/create/create'))
+router.post('/v1/users/create', jsonParser, require('./users/create/create'))
 
 // Swagger route
 router.use('/v1/docs', require('./utils/swagger/routeHandler'), swaggerUi.serve, swaggerUi.setup(swaggerDocument));
--- src/users/create/create.js
+++ src/users/create/create.js
@@ -1,6 +1,17 @@
+const mySql = require('../../../data/mysql/mysql')
+const helper = require('../_helper/helper')
+const sql = require('../_helper/sqlQuery')
+
 async function create(req,res){
+    const { email, password, dateOfBirth, mothersMaidenName } = req.body
+
     try {
-        res.send("hello")
+        res.send([
+            email,
+            password,
+            dateOfBirth,
+            mothersMaidenName
+        ])
     } catch (error) {
         console.log(error)
     }
