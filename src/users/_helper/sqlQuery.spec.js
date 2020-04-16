2962545dda4e243adb1d47f685703a93872be34e
Austin Finn <austin.finn@qantasloyalty.com>
2020-04-16 12:22:16 +1000

    Add tests for sql scripts

--- src/users/_helper/sqlQuery.spec.js
+++ src/users/_helper/sqlQuery.spec.js
@@ -1,13 +1,30 @@
 const expect  = require('chai').expect;
 const chai = require('chai')
 
-const { getUserEmailsQuery } = require('./sqlQuery')
+const sql = require('./sqlQuery')
 
 describe('Users SQL queries', () => {
+    const email = "fake@email.com"
     it(`should return the correct query for getUserEmailsQuery()`, () => {
-        const resultWithExtraSpaces = getUserEmailsQuery()
+        const resultWithExtraSpaces = sql.getUserEmailsQuery()
         const result = resultWithExtraSpaces.replace(/\s\s+/g, ' ')
 
         expect(result).to.contain(`SELECT user_id, email FROM users;`)
+    })
+
+    it(`should return the correct query for insertUser()`, () => {
+        const dateOfBirth="1999-12-31"
+
+        const resultWithExtraSpaces = sql.insertUser(email, dateOfBirth)
+        const result = resultWithExtraSpaces.replace(/\s\s+/g, ' ')
+
+        expect(result).to.contain(`INSERT INTO users (email, date_of_birth) VALUES('fake@email.com','1999-12-31');`)
+    })
+
+    it(`should return the correct query for selectUserIdByEmail()`, () => {
+        const resultWithExtraSpaces = sql.selectUserIdByEmail(email)
+        const result = resultWithExtraSpaces.replace(/\s\s+/g, ' ')
+
+        expect(result).to.contain(`SELECT user_id as userId FROM users WHERE email='fake@email.com';`)
     })
 })
