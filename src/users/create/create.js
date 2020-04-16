fd3ce6c052ea1ce8ba92602e1f45f33910e37a44
Austin Finn <austin.finn@qantasloyalty.com>
2020-04-16 12:40:18 +1000

    Add comments

--- src/users/create/create.js
+++ src/users/create/create.js
@@ -6,13 +6,16 @@ async function create(req,res){
     const { email, password, dateOfBirth, mothersMaidenName } = req.body
 
     try {
+        // insert the new user into the db
         let sqlQuery = sql.insertUser(email, dateOfBirth)
         await mySql.executeQuery(sqlQuery)
 
+        // get the 'user_id' for the newly create user
         sqlQuery = sql.selectUserIdByEmail(email)
         const dbResult = await mySql.executeQuery(sqlQuery)
         const { userId } = dbResult[0]
 
+        // save important login details for test accounts to the Google Sheets doc
         await helper.saveUserDetailsToGoogleSheets(userId, password, mothersMaidenName)
 
         res.send({
