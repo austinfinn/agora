991ea753c38d93fe6eade65823219f6c5da21e9f
Austin Finn <austin.finn@qantasloyalty.com>
2020-04-16 10:59:54 +1000

    Get Redis running locally with app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const redis = require('redis')
 const app = express()
 const port = 4001
 
@@ -7,4 +8,29 @@ app.use(routes)
 
 app.use(`/public`, express.static('public'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const connectToRedis = () => {
+    const client = redis.createClient("redis://localhost:6379")
+
+    return new Promise((resolve, reject) => {
+        client.on('connect', () => {
+            resolve(client)
+        });
+        client.on('error', (err) => {
+            reject(err)
+        })
+    })
+}
+
+connectToRedis().then((client) => {
+    app.locals.redis = client;
+    // set port
+    app.listen(port, function () {
+        
+        console.log("")
+        console.log(`Agora app running on http://localhost:${port}`)
+        console.log(`Redis is running on redis://localhost:6379`)
+        console.log("")
+    });
+}, (error) => {
+    console.error(`Unable to connect to Redis DB with error: `, error);
+})
