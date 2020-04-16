c61660f384f2f6b38a70de95f1c83165e77aade0
Austin Finn <austin.finn@qantasloyalty.com>
2020-04-16 11:06:53 +1000

    Move caching time to config.js and create a new variable to make it clearer what the first argument for setex() is

--- src/users/loginCredentials/loginCredentials.js
+++ src/users/loginCredentials/loginCredentials.js
@@ -1,9 +1,12 @@
 const mySql = require('../../../data/mysql/mysql')
 const helper = require('../_helper/helper')
 const sql = require('../_helper/sqlQuery')
+const config = require('../../config')
+const { cachingTime } = config.users.loginCredentials
 
 async function loginCredentials(req,res){
     const redisClient = req.app.locals.redis
+    const key = req.path
 
     try {
         const sqlQuery = sql.getUserEmailsQuery()
@@ -12,7 +15,7 @@ async function loginCredentials(req,res){
         const loginCredentials = await helper.getCredentials(dbUsers)
 
         // save details to Redis
-        redisClient.setex(req.path, 15, JSON.stringify(loginCredentials))
+        redisClient.setex(key, cachingTime, JSON.stringify(loginCredentials))
 
         res.send(loginCredentials)
     } catch (error) {
