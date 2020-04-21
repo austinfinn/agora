372d3a497e75a3030b0c10acd8545b3828606e06
Austin Finn <austin.finn@qantasloyalty.com>
2020-04-22 09:16:37 +1000

    Modify the Swagger page's "host" property to use the correct port.
    When started the application with Docker it was using the wrong port so all the endpoints on the Swagger page stopped working

--- src/utils/swagger/customizePage.js
+++ src/utils/swagger/customizePage.js
@@ -1,8 +1,10 @@
 const swaggerDocument = require('./swagger.json')
 const appPackage = require('../../../package.json')
+require('dotenv').config()
 
 function customizePage(req, res, next) {
     swaggerDocument.info.version = appPackage.version
+    swaggerDocument.host = `localhost:${process.env.PORT}`
 
     req.swaggerDoc = swaggerDocument;
     next();
