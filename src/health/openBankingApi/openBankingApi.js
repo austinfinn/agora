571880741dbab26d03cf31efff220cea30bf2727
Austin Finn <austin.finn@qantasloyalty.com>
2020-04-22 23:24:47 +1000

    Refactor the code to return a status of 'DOWN' when the underlying endpoint returns a non-200 status code

--- src/health/openBankingApi/openBankingApi.js
+++ src/health/openBankingApi/openBankingApi.js
@@ -4,24 +4,26 @@ const utils = require('../../utils/utils')
 
 async function openBankingApi(req,res){
     const { bank } = req.params
-    let status = ""
 
     try {
         const url = utils.getProductsUrl(bank)
-        const result = await nr.getRequest(url)
-        
-        if(result.status == 200){
-            status = 'UP'
-        } else {
-            status = 'DOWN'
-        }
 
+        // if this doesn't throw an error i.e. response has a status code of 200, assume endpoint is UP
+        await nr.getRequest(url)
+    
         res.send({
-            message: status
+            message: "UP"
         })
     } catch (error) {
-        const response = eh.errorsHandler(error) 
-        res.status(response.statusCode).send(response.body)
+        // if an error is thrown and it contains a Repsonse status code then assume the endpoint is DOWN
+        if(error.response.status){
+            res.status(200).send({
+                message: "DOWN"
+            })
+        } else {
+            const response = eh.errorsHandler(error) 
+            res.status(response.statusCode).send(response.body)
+        }
     }
 }
 
