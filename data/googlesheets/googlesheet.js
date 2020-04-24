698c9a86581c20971f7f3d29de2b91e9025d176a
Austin Finn <austin.finn@qantasloyalty.com>
2020-04-24 22:51:05 +1000

    Refactor the how the function to make it easier to test

--- data/googlesheets/googlesheet.js
+++ data/googlesheets/googlesheet.js
@@ -1,9 +1,9 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+const google = require('google-spreadsheet');
 const credentials = require('./agora-1586910581121-5e0b7a5f1034.json')
 
 async function connect() {
-    const doc = new GoogleSpreadsheet('1p7QVbO-6HqKntbTCjH43o3DQx7EBiXKE1zDSpm5DbZ8');
-    
+    const doc = new google.GoogleSpreadsheet('1p7QVbO-6HqKntbTCjH43o3DQx7EBiXKE1zDSpm5DbZ8')
+
     // use service account credentials
     await doc.useServiceAccountAuth(credentials);
 
