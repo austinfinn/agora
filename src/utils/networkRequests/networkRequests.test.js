8a2836d8c9b7169a8f33d55145d8afea24169bd7
Austin Finn <astin21@gmail.com>
2020-06-03 18:09:01 +1000

    Rename all test files by swapping the term '.spec' for '.test'

--- src/utils/networkRequests/networkRequests.test.js
+++ src/utils/networkRequests/networkRequests.test.js
@@ -10,15 +10,15 @@ const nr = require('./networkRequests')
 describe('getRequest()', () => {
     it(`should check the details of Request`, async () => {
         const result = { data:"fake data" }
-        const inputUrl='https://api.fake.com/path/for/testing'
+        const url='https://api.fake.com/path/for/testing'
         const stubAxiosGetRequest = sinon.stub(axios.default,'get').returns(result)
 
-        await nr.getRequest(inputUrl)
+        await nr.getRequest(url)
 
         const outputUrl = stubAxiosGetRequest.firstCall.args[0]
         const options = stubAxiosGetRequest.firstCall.args[1]
 
-        expect(outputUrl).to.equal(inputUrl)
+        expect(outputUrl).to.equal(url)
         expect(options.headers['x-v']).to.equal("1")
 
         sinon.assert.calledOnce(stubAxiosGetRequest)
