60b8972abd0946c7e15c1deb6610963c37fcb3b2
Austin Finn <astin21@gmail.com>
2021-01-19 21:25:34 +1100

    Order the protocols on the Swagger page

--- src/utils/swagger/customizePage.js
+++ src/utils/swagger/customizePage.js
@@ -8,10 +8,12 @@ function customizePage(req, res, next) {
     if (global.localDevelopment){
         // remove the protocol and just use the hostname
         swaggerDocument.host = global.localhost.split('//')[1]
+        swaggerDocument.schemes = ["http","https"]
     } 
     else {
         // remove the protocol and just use the hostname
         swaggerDocument.host = process.env.HEROKU_HOST.split('//')[1]
+        swaggerDocument.schemes = ["https","http"]
     }
 
     req.swaggerDoc = swaggerDocument;
